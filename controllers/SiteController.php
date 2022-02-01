2685cfa49732c6d7f4a7db46573eecf1f7fa82bb
xavier.velez3132 <xavier.velez3132@gmail.com>
2022-02-01 18:47:47 -0500

    basic login and signup with database

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\SignupForm;
 use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
@@ -125,4 +126,16 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionSignup(){
+        $model = new SignupForm();
+
+        if($model->load(Yii::$app->request->post()) && $model->signup()){
+            return $this->redirect(Yii::$app->homeUrl);
+        }
+        return $this->render('signup',[
+            'model' => $model
+        ]);
+    }
+
 }
